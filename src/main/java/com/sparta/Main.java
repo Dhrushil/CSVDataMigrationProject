cb39902a21e5adeab1ee74fc2d9e469ed06caeab
Jamie Scofield <jamies_1048@outlook.com>
2022-07-11 16:59:50 +0100

    made read file to return

--- src/main/java/com/sparta/Main.java
+++ src/main/java/com/sparta/Main.java
@@ -8,31 +8,5 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
 
-        HashMap<Integer, Employee> collections = new HashMap<>();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/csv/EmployeeRecords1.csv"));
-            reader.readLine(); // this will read the first line
-            String data=null;
-            while ((data = reader.readLine()) != null){ //loop will run from 2nd line
-                String[] dataArray = data.split(",");
-                int employeeID = Integer.parseInt(dataArray[0]);
-                String namePrefix = dataArray[1];
-                String firstName = dataArray[2];
-                char middleInitial = dataArray[3].charAt(0);
-                String lastName = dataArray[4];
-                char gender = dataArray[5].charAt(0);
-                String email = dataArray[6];
-                String dateOfBirth = dataArray[7];
-                String dateOfJoining = dataArray[8];
-                int salary = Integer.parseInt(dataArray[9]);
-
-                collections.put(employeeID, new Employee(employeeID, namePrefix, firstName, middleInitial, lastName, gender, email, dateOfBirth, dateOfJoining, salary));
-            }
-        }catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
     }
 }
--- src/main/java/com/sparta/controller/IOReader.java
+++ src/main/java/com/sparta/controller/IOReader.java
@@ -1,21 +1,45 @@
 package com.sparta.controller;
 
+import com.sparta.model.Employee;
+
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
 
 public class IOReader {
-    public static void readFile(String filename) {
+    public static HashMap<Integer, Employee> readFile(String filename) {
 
+        HashMap<Integer, Employee> file = new HashMap<>();
         try {
+            BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/csv/EmployeeRecords1.csv"));
+            reader.readLine(); // this will read the first line
+            String data=null;
+            while ((data = reader.readLine()) != null){ //loop will run from 2nd line
+                String[] dataArray = data.split(",");
+                int employeeID = Integer.parseInt(dataArray[0]);
+                String namePrefix = dataArray[1];
+                String firstName = dataArray[2];
+                char middleInitial = dataArray[3].charAt(0);
+                String lastName = dataArray[4];
+                char gender = dataArray[5].charAt(0);
+                String email = dataArray[6];
+                String dateOfBirth = dataArray[7];
+                String dateOfJoining = dataArray[8];
+                int salary = Integer.parseInt(dataArray[9]);
 
+                file.put(employeeID, new Employee(employeeID, namePrefix, firstName, middleInitial, lastName, gender, email, dateOfBirth, dateOfJoining, salary));
+            }
 
-            FileReader fileReader = new FileReader(filename);
-
-
-
-        }catch (FileNotFoundException e){
+        }catch (FileNotFoundException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
+        return file;
     }
+
 }
+
