b03f458097ffd6e3335fdfadd215ce7578c09540
DogukhanK <dogukhan.k@gmail.com>
2022-07-12 15:49:45 +0100

    UI Class

---
+++ src/main/java/com/sparta/view/UserInterfaceView.java
@@ -0,0 +1,10 @@
+package com.sparta.view;
+
+public class UserInterfaceView {
+    public void displayReading() {
+        System.out.println("Clean records = ");
+        System.out.println("Duplicates found = ");
+        System.out.println("Record with missing fields = ");
+        //System.out.println("Records with questionable fields = ");
+    }
+}
