a8ab1c552d04fad2bc04730c0e86152c01c4f60d
ABDURSHAZAM <85063702+ABDURSHAZAM@users.noreply.github.com>
2022-07-12 17:22:46 +0100

    more test

--- src/main/java/com/sparta/controller/TestClass.java
+++ src/main/java/com/sparta/controller/TestClass.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,7 +32,33 @@ class TestClass {
     }
 
     @Test
-    void writeFile() {
+    void testCleanFile(){
+        CleaningController clean = new CleaningController();
+        //HashMap<Integer, Employee> test = clean.cleanFile();
+    }
 
+    @Test
+    void writeFile() {
+<<<<<<< Updated upstream
+
+=======
+        HashMap<Integer, Employee> testMap = IOReader.readFile("src/main/resources/csv/EmployeeRecords1.csv");
+        IOReader.writeFile("newTempFile.csv", testMap);
+        Assertions.assertTrue(new File("newTempFile.csv").isFile()); //check if new file exists
+
+        //HashMap<Integer, Employee> newMap = IOReader.readFile("newTempFile.csv");
+
+
+        /*Assertions.assertEquals("serafina.bumgarner@exxonmobil.com", newMap.get(198429).getEmail());
+        Assertions.assertEquals("Fri Sep 09 00:00:00 BST 1983", newMap.get(198429).getDateOfBirth().toString());
+        Assertions.assertEquals("Mrs.", newMap.get(198429).getNamePrefix());
+        Assertions.assertEquals("Serafina", newMap.get(198429).getFirstName());
+        Assertions.assertEquals('I', newMap.get(198429).getMiddleInitial());
+        Assertions.assertEquals("Bumgarner", newMap.get(198429).getLastName());
+        Assertions.assertEquals('F', newMap.get(198429).getGender());
+        Assertions.assertEquals(198429, newMap.get(198429).getEmployeeID());
+        Assertions.assertEquals(69294, newMap.get(198429).getSalary());
+        Assertions.assertEquals("Wed Jan 02 00:00:00 GMT 2008", newMap.get(198429).getDateOfJoining().toString());*/
+>>>>>>> Stashed changes
     }
 }
