87da1ad4b43df343c43a4bdecf6c656bee5c3da8
ABDURSHAZAM <85063702+ABDURSHAZAM@users.noreply.github.com>
2022-07-12 14:02:54 +0100

    First Pass on JUnit tests

---
+++ src/main/java/com/sparta/controller/TestClass.java
@@ -0,0 +1,16 @@
+package com.sparta.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestClass {
+
+    @Test
+    void readFile() {
+    }
+
+    @Test
+    void writeFile() {
+    }
+}
