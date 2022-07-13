de6197d4db4111e04e18c00b242a6b843b054e0c
Jamie Scofield <jamies_1048@outlook.com>
2022-07-13 13:33:22 +0100

    added the start to create a table method. (needs to be completed)

--- src/main/java/com/sparta/controller/ReadWriteCon.java
+++ src/main/java/com/sparta/controller/ReadWriteCon.java
@@ -10,6 +10,20 @@ import java.sql.Statement;
 import java.util.HashMap;
 
 public class ReadWriteCon {
+
+    public static final String createtable = "CREATE TABLE Employee_Data " +
+            "(Emp_ID INTEGER not NULL," +
+            "Name_Prefix varchar(50)," +
+            "First_Name varchar(50)," +
+            "Middle_Initial varchar(50)," +
+            "Last_Name varchar(50)," +
+            "Gender varchar(50)," +
+            "E-Mail varchar(50)," +
+            "Date_of_Birth DATE," +
+            "Date_of_Joining DATE," +
+            "Salary INTEGER";
+
+
     public HashMap<Integer,Employee> readingRecords1() {
         HashMap<Integer,Employee> map = new HashMap<>();
         map = IOReader.readFile("src/main/resources/csv/EmployeeRecords1.csv");
@@ -35,17 +49,8 @@ public class ReadWriteCon {
 
         try {
             Statement statement = conn.createStatement();
-            statement.
-            ResultSet rs = statement.execute("CREATE TABLE(Emp_ID int," +
-                    "Name_Prefix varchar(50)," +
-                    "First_Name varchar(50)," +
-                    "Middle_Initial varchar(50)," +
-                    "Last_Name varchar(50)," +
-                    "Gender varchar(50)," +
-                    "E-Mail varchar(50)," +
-                    "Date_of_Birth DATE," +
-                    "Date_of_Joining DATE," +
-                    "Salary int");
+            statement.execute(createtable);
+
 
 
         } catch (SQLException e) {
