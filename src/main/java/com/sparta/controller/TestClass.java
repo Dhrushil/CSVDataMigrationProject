396f74ab0b840d29b05bf2758e7619f8576642af
ABDURSHAZAM <85063702+ABDURSHAZAM@users.noreply.github.com>
2022-07-14 15:36:35 +0100

    Cleaning tests

--- src/main/java/com/sparta/controller/TestClass.java
+++ src/main/java/com/sparta/controller/TestClass.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.text.ParseException;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -35,7 +36,18 @@ class TestClass {
     @Test
     void testCleanFile(){
         CleaningController clean = new CleaningController();
-        //HashMap<Integer, Employee> test = clean.cleanFile();
+        try {
+            HashMap<Integer, Employee> test = clean.cleanFile("src/main/resources/csv/EmployeeRecords1.csv");
+
+            Assertions.assertEquals("serafina.bumgarner@exxonmobil.com", test.get(198429).getEmail());
+            Assertions.assertEquals("Mrs.", test.get(198429).getNamePrefix());
+            Assertions.assertEquals("Serafina", test.get(198429).getFirstName());
+            Assertions.assertEquals('I', test.get(198429).getMiddleInitial());
+            Assertions.assertEquals("Bumgarner", test.get(198429).getLastName());
+
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
@@ -44,28 +56,12 @@ class TestClass {
         HashMap<Integer, Employee> testMap = IOReader.readFile("src/main/resources/csv/EmployeeRecords1.csv");
         IOReader.writeFile("newTempFile.csv", testMap);
         Assertions.assertTrue(new File("newTempFile.csv").isFile()); //check if new file exists
-
-        //HashMap<Integer, Employee> newMap = IOReader.readFile("newTempFile.csv");
-
-
-        /*Assertions.assertEquals("serafina.bumgarner@exxonmobil.com", newMap.get(198429).getEmail());
-        Assertions.assertEquals("Fri Sep 09 00:00:00 BST 1983", newMap.get(198429).getDateOfBirth().toString());
-        Assertions.assertEquals("Mrs.", newMap.get(198429).getNamePrefix());
-        Assertions.assertEquals("Serafina", newMap.get(198429).getFirstName());
-        Assertions.assertEquals('I', newMap.get(198429).getMiddleInitial());
-        Assertions.assertEquals("Bumgarner", newMap.get(198429).getLastName());
-        Assertions.assertEquals('F', newMap.get(198429).getGender());
-        Assertions.assertEquals(198429, newMap.get(198429).getEmployeeID());
-        Assertions.assertEquals(69294, newMap.get(198429).getSalary());
-        Assertions.assertEquals("Wed Jan 02 00:00:00 GMT 2008", newMap.get(198429).getDateOfJoining().toString());*/
-
     }
 
     @Test
     void testUI(){
         UserInterfaceView ui = new UserInterfaceView();
         HashMap<Integer, Employee> testMap = IOReader.readFile("src/main/resources/csv/EmployeeRecords1.csv");
-
         //Assertions.assertEquals(9943, );
         ui.displayReading(testMap, 0);
     }
