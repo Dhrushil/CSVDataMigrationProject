51965eeeb8dce84778801e7f5f13bd301428b98c
dhrushilsedani <dhrushilsedani@hotmail.co.uk>
2022-07-15 14:25:29 +0100

    added threading

--- src/main/java/com/sparta/MyThread.java
+++ src/main/java/com/sparta/MyThread.java
@@ -1,6 +1,8 @@
 package com.sparta;
 
 import com.sparta.controller.CleaningController;
+import com.sparta.controller.IOReader;
+import com.sparta.controller.ReadWriteCon;
 import com.sparta.model.Employee;
 
 import java.text.ParseException;
@@ -11,62 +13,58 @@ import java.util.Iterator;
 public class MyThread extends Thread{
 
     //define the Total No.Of Threads needed
-    public static final int TOTAL_THREADS = 5;
+    public static final int TOTAL_THREADS = 10;
 
     public final static Object obj = new Object();
 
     public volatile static Iterator keyIterator;   // iterate through keys
 
+    CleaningController clean = new CleaningController();
+
     public static HashMap<Integer, Employee> cMap;
 
+    ReadWriteCon maincon = new ReadWriteCon();
+
+    String fileName;
 
 
 
     int threadNo;
     static volatile int counter = 1;
 
-    public MyThread(int threadNo) throws ParseException {
+    public MyThread(int threadNo, String fileName){
         this.threadNo= threadNo;
-        CleaningController clean = new CleaningController();
-
-        cMap = clean.cleanFile("src/main/resources/csv/EmployeeRecords1.csv");
-
-//        Date date = new Date();
-//        cMap.put(10, new Employee(10,"d","name1",'s',"dsadsa",'F',"dsadas@sdf.com", date, date, 1000));
-//        cMap.put(15, new Employee(15,"d","name2",'s',"dsadsa",'F',"dsadas@sdf.com", date, date, 1000));
-//        cMap.put(20, new Employee(20,"d","name3",'s',"dsadsa",'F',"dsadas@sdf.com", date, date, 1000));
-//        cMap.put(25, new Employee(25,"d","name4",'s',"dsadsa",'F',"dsadas@sdf.com", date, date, 1000));
-//        cMap.put(30, new Employee(30,"d","name5",'s',"dsadsa",'F',"dsadas@sdf.com", date, date, 1000));
-//        cMap.put(35, new Employee(35,"d","name6",'s',"dsadsa",'F',"dsadas@sdf.com", date, date, 1000));
-//        cMap.put(40, new Employee(40,"d","name7",'s',"dsadsa",'F',"dsadas@sdf.com", date, date, 1000));
-//        cMap.put(45, new Employee(45,"d","name8",'s',"dsadsa",'F',"dsadas@sdf.com", date, date, 1000));
-//        cMap.put(50, new Employee(50,"d","name9",'s',"dsadsa",'F',"dsadas@sdf.com", date, date, 1000));
-
-        this.keyIterator = cMap.keySet().iterator();
-
+        this.fileName = fileName;
     }
 
     @Override
     public void run(){
 
+        try {
+            cMap = clean.cleanFile(fileName);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        keyIterator = cMap.keySet().iterator();
+
         //in a synchronized block to acquire lock
         synchronized (obj) {
 
-            while(counter<=9){
+            while(counter<=cMap.size()-100){
 
                 if(counter == threadNo || (counter%TOTAL_THREADS == threadNo) ||
                         ((counter%TOTAL_THREADS == 0) && (TOTAL_THREADS == threadNo))){
 
                     //Display the output as desired
-                    String s = this.threadNo + " printing" + " " + counter++;
-                    System.out.println(cMap.get(keyIterator.next()).getFirstName());
+                   // String s = this.threadNo + " printing" + " " + counter++;
+                    counter++;
+                    maincon.updateTableWithThreads(cMap.get(keyIterator.next()));
 
-                    //then
-                    //notify
+
                     obj.notifyAll();
                 }else{
 
-                    //current thread not eligible for printing the current counter value, so wait till its notified
                     try {
                         obj.wait();
                     } catch (InterruptedException e) {
@@ -83,8 +81,11 @@ public class MyThread extends Thread{
         /*
          * Creating as many threads as needed.
          */
-        for(int i = 1; i<=TOTAL_THREADS;i++){
-            MyThread th = new MyThread(i);
+
+
+
+        for(int i = 1; i<=MyThread.TOTAL_THREADS;i++){
+            MyThread th = new MyThread(i,"src/main/resources/csv/EmployeeRecords1.csv");
             th.start();
         }
     }
